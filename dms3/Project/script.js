3c2ac938abb4fda6b91e9db8c7414fabe831e5c6
Sakura <s4088734@student.rmit.edu.au>
2025-05-06 22:12:45 +1000

    started brush size options, assign id to pins

--- dms3/Project/script.js
+++ dms3/Project/script.js
@@ -5,7 +5,6 @@ let stage = new Konva.Stage({
 });
 
 let colourpicker = document.getElementById("colour");
-console.log(colourpicker);
 colourpicker.addEventListener("input", pickedColour);
 
 let newColour = "black";
@@ -23,16 +22,12 @@ function pickedColour(e){
 
 let stringWidth = 1;
 
-function brushSize(){
-};
-
-// click on one of the three divs
-// line width will change based on which is clicked
-
 let stringLayer = new Konva.Layer();
 stage.add(stringLayer);
 let newLayer = new Konva.Layer();
 stage.add(newLayer);
+
+let pinId = 0;
 
 let pin = {
     radius: 15,
@@ -42,20 +37,47 @@ let pin = {
     y: stage.height()/2,
     shadowBlur: 5,
     shadowOpacity: 0.3,
+    id: pinId,
 };
 
 let x1 = pin.x;
 let y1 = pin.y;
+var pinPos = [x1, y1];
 
+//assign pin id upon creating, from 1
 document.getElementById("pin").addEventListener("click",newPin);
-
 function newPin(){
     let addPin = new Konva.Circle(pin);
+    pinId = pinId + 1;
+    console.log(pinId);
     newLayer.add(addPin);
     console.log(addPin); 
 }
 
-let stringPos = [0, 0, 20, 20];
+let stringPos = [0, 0, 50, 50];
+
+function updPinPos() {
+    pinPos.on()
+    console.log(pin.x);
+    console.log(pin.y);
+}
+
+// click on one of the three divs
+// line width will change based on which is clicked
+
+document.getElementById("three").addEventListener("click",brushSize);
+document.getElementById("two").addEventListener("click",brushSize);
+document.getElementById("one").addEventListener("click",brushSize);
+
+function brushSize(){
+    if (("button").id = "three") {
+        stringWidth = 12
+    } else if (("button").id = "two") {
+        stringWidth = 8
+    } else {
+        stringWidth = 3
+    };
+};
 
 let string = {
     stroke: newColour,
@@ -65,12 +87,6 @@ let string = {
     shadowBlur: 7,
     shadowOpacity: 0.2,
     strokeWidth: stringWidth,
-}
-
-string.addEventListener("dragmove", updPin);
-
-function updPin() {
-    string.points[x1, y1];   
 }
 
 document.getElementById("three").addEventListener("click",newString);
