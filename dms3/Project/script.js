e523db764fa34ac6f662aa10da149ef2c4b61e4c
Sakura <s4088734@student.rmit.edu.au>
2025-05-14 13:42:44 +1000

    14th progress1

--- dms3/Project/script.js
+++ dms3/Project/script.js
@@ -36,6 +36,35 @@ stage.add(stringLayer);
 let newLayer = new Konva.Layer();
 stage.add(newLayer);
 
+// click on one of the three divs
+// line width will change based on which is clicked
+/*three.addEventListener("click",brushSize);
+two.addEventListener("click",brushSize);
+one.addEventListener("click",brushSize);
+
+// always defaults to 3 which means what. the div being clicked isn't the third or second???
+function brushSize(clicked){
+    if (clicked == three) {
+        string.strokeWidth = 12
+    } else if (clicked == two) {
+        string.strokeWidth = 8
+    } else {
+        string.strokeWidth = 3
+    };
+};*/
+
+// funct x(y) {swidth = y}
+// event => {x(width for corresponding button)}
+function brushSize(sWidth) {
+    string.strokeWidth = sWidth;
+}
+three.addEventListener("click", () => {
+    brushSize(12);});
+two.addEventListener("click", () => {
+    brushSize(8);});
+one.addEventListener("click", () => {
+    brushSize(3);});
+
 let pinId = 0;
 let stringId = 0;
 
@@ -50,8 +79,6 @@ let pin = {
     id: pinId,
 };
 
-// use a for loop to log all pin id's in a single array
-// let allPins = Konva.Circle(pin);
 let createdPins = [];
 
 document.getElementById("pin").addEventListener("click",newPin);
@@ -61,6 +88,7 @@ function newPin(){
     pinId = pinId + 1;
     newLayer.add(addPin);
     createdPins.push(addPin);
+    addPin.on("dragmove", stringUpd);
 };
 
 /* attempted pin select
@@ -73,70 +101,66 @@ function selection() {
     selectedPins.push();
     console.log(selectedPins);
 }*/
-
 //let pinAdding = newPin();
+
 let stringPos = [0, 0, 20, 20];
-
-// click on one of the three divs
-// line width will change based on which is clicked
-three.addEventListener("click",brushSize);
-two.addEventListener("click",brushSize);
-one.addEventListener("click",brushSize);
-
-// setting stroke width of string
-function brushSize(widthSetting){
-    if (widthSetting === three) {
-        string.strokeWidth = 12
-    } else if (widthSetting === two) {
-        string.strokeWidth = 8
-    } else {
-        string.strokeWidth = 3
-    };
-};
 
 let string = {
     stroke: newColour,
     points: stringPos,
-    x: stage.width()/2,
-    y: stage.height()/2,
+    //x: stage.width()/2,
+    //y: stage.height()/2,
     shadowBlur: 7,
     shadowOpacity: 0.2,
     strokeWidth: stringWidth,
     //id: stringId,
 }
 
-three.addEventListener("click",newString);
+/*three.addEventListener("click",newString);
 two.addEventListener("click",newString);
 one.addEventListener("click",newString);
+function newString(){
+    stringUpd();
+    let addString = new Konva.Line(string);
+    //changed to "if" to only add string if there are at least two pins on the board.
+    if (createdPins.length > 1) {
+        stringLayer.add(addString);
+    };}*/
+// move down?
 
 // function for updating strings, look through pinarray. call when moving + creating pins
-function newString(){
-    let addString = new Konva.Line(string);
-    if (createdPins.length > 1) {
-        stringLayer.add(addString)};
-    //stringId = stringId + 1;
-}
-
-document.getElementById("pin").addEventListener("dragmove", stringUpd);
-
 function stringUpd() {
-    //look through createdPins.
-    // look for min 2 in array (if pinarray.length) /
-    console.log(createdPins.length);
-    //i will refer to the second pin in the array. if 1 is lower than the number of pins created, i keeps going up until it reaches the number of pins created. pin1 is the second latest pin. while pin2 is the current latest pin
+    //[i] will refer to the second pin in the array. if [i] is lower than the number of pins created, [i] keeps going up until it reaches the number of pins created. 
+    // pin1 is the second latest pin. while pin2 is the latest pin
     for(let i = 1; i < createdPins.length; i++) {
     let pin1 = createdPins[i - 1];
     let pin2 = createdPins[i];
     if (createdPins.length > 1) {
         console.log(pin1.x());
         // the x of everything that has been pin1 seems to get logged.
-        string.x = pin1.x();
-        string.y = pin1.y();
+        //string.x = pin1.x(); 
+        //string.y = pin1.y();
         string.points = [pin1.x(), pin1.y(), pin2.x(), pin2.y()];
-        // points of string correspond to pin position, but only in some unknown way???
-        // find all strings and remove them?
-        canvas.remove(stringLayer);
-    }};}
+    }};
+    //newstring
+
+    function newString(){
+    //stringUpd();
+    let addString = new Konva.Line(string);
+    //changed to "if" to only add string if there are at least two pins on the board.
+    if (createdPins.length > 1) {
+        stringLayer.add(addString);
+    };}
+    /*if (createdPins.length > 1) {
+        // loop for finding and removing all lines?
+        for(let i = 1; i < createdPins.length; i++) {
+            let allStrings = stringLayer.find("Line");
+            canvas.remove(allStrings);
+        }
+    };*/
+};
+
+//pin.addEventListener("drag", stringUpd); - old
 
 //array.from(doc.getbyclass,, for each (element
 //element.add event)
@@ -146,7 +170,8 @@ Array.from(document.getElementsByClassName("width")).forEach(wBtn => {
 })
 */
 
-/*let stringAdding = newString();
+/* attempted adding string (old)
+let stringAdding = newString();
 function updPinPos() {
     /*pinAdding.forEach((pin) => {
         const node = newLayer.findOne(pin.id);
@@ -157,12 +182,11 @@ function updPinPos() {
         const string = stringLayer.findOne("#" + string.id);
         fromNode = stringLayer.findOne("#" + string.from);
     })}*/
-// string.points([0, 0, pinPos])
 
 // Undo function
 // stage.getContext is not a function.
 // changed "stage" to "canvas"
-let undoStack = [];
+/*let undoStack = [];
 let currentState = canvas.toDataURL();
 let undoLimit = 5;
 function saveUndoState() {
@@ -194,10 +218,6 @@ function undo() {
 }
 document.getElementById("undo").addEventListener("click", () => {
     undo();
-});
+}); */
 
-// importing images
-document.getElementById("upload").addEventListener("click", addImg);
-function addImg() {
-
-}
+// importing images from input=file ???
